76839e6f2d5361880b1bb5256b5aa3675c5fd417
Thomas Lapierre <thomaslapierre@orange.fr>
2020-04-06 11:47:15 +0200

    Page d'accueil

--- pages/index.js
+++ pages/index.js
@@ -3,57 +3,42 @@ import Head from 'next/head'
 const Home = () => (
   <div className="container">
     <Head>
-      <title>Create Next App</title>
+      <title>QuelJeu</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
     <main>
       <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
+        Bienvenue sur <a href="">Quel Jeu</a>
       </h1>
 
       <p className="description">
-        Get started by editing <code>pages/index.js</code>
+        Trouve le jeu vidéo qui te convient parfaitement ! <code>Bonne découverte</code>
       </p>
+
 
       <div className="grid">
         <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
+          <h3>Trouve ton jeu &rarr;</h3>
+          <p>Grâce à mon algorithme et une base de données remplie de milliers de jeux vidéos différents,
+              cet outil trouvera les meilleurs jeux fait pour toi.</p>
         </a>
 
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
+          <a href="https://nextjs.org/docs" className="card">
+              <h3>Catalogue des jeux &rarr;</h3>
+              <p>Retrouve la liste de tous les jeux vidéos. Tu peux effectuer des recherches très
+              précises de manière simple et rapide !</p>
+          </a>
 
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with ZEIT Now.
-          </p>
-        </a>
       </div>
     </main>
 
     <footer>
       <a
-        href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        href=""
         target="_blank"
-        rel="noopener noreferrer"
       >
-        Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
+        Réalisé par Thomas Lapierre
       </a>
     </footer>
 
