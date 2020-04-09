da00abdd6e5f6d81555bf2a3e6dc2208a37e55d4
Thomas Lapierre <thomaslapierre@orange.fr>
2020-04-09 19:02:37 +0200

    Création du slider, affichage de l'année en temps réel, responsive

--- pages/components/PageDeQuestions/PageDeQuestionSlider.js
+++ pages/components/PageDeQuestions/PageDeQuestionSlider.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Slider, Switch } from 'antd';
+import {Container, Label} from "semantic-ui-react";
 
 
 class PageDeQuestionSlider extends React.Component {
@@ -7,8 +8,11 @@ class PageDeQuestionSlider extends React.Component {
     constructor(props) {
         super(props);
         this.state={
-            date:[]
+            min:1990,
+            max:2020,
+            rangedepart:[2010,2020]
         }
+
     }
 
     changementDate(val){
@@ -19,11 +23,47 @@ class PageDeQuestionSlider extends React.Component {
 
 
     render() {
+        let date;
+        if(this.state.date!=null){
+            date = this.state.date;
+        }else{
+            date = this.state.rangedepart
+        }
+
+        let affichagedate;
+
+        if(date[0]==date[1]){
+            affichagedate=<div className="ui huge circular labels "><Label className="ui blue basic label">
+                {date[0]}
+            </Label></div>
+
+        }else{
+                affichagedate=<div className="ui huge circular labels ">
+
+                <Label className="ui blue basic label">
+                    {date[0]}
+                </Label>
+                    <Label className="ui blue basic label">
+                        {date[1]}
+                    </Label></div>
+         }
+
         return (
             <div>
-                <div style={{width: 500+'px'}}>
-                    <Slider range defaultValue={[2010, 2020]} min={1990} max={2020} onChange={(value)=> this.changementDate(value)}/>
-                </div>
+                <h1 className="ui header center aligned">Année</h1>
+
+                    <Slider range defaultValue={[this.state.rangedepart[0], this.state.rangedepart[1]]}
+                            min={this.state.min}
+                            max={this.state.max}
+                            tooltipVisible={false}
+                            onChange={(value)=> this.changementDate(value)}
+                    />
+                <Container textAlign='center'>
+
+                    {affichagedate}
+
+                 </Container>
+
             </div>
         );
     }
