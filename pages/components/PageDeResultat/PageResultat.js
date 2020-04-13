304c7ca7268e84ae148dcc423bb2b13bf93bb602
Thomas Lapierre <thomaslapierre@orange.fr>
2020-04-13 15:25:47 +0200

    Correction bug déploiement

--- pages/components/PageDeResultat/PageResultat.js
+++ pages/components/PageDeResultat/PageResultat.js
@@ -13,7 +13,7 @@ const PageResultat = ({req}) => {
     const [posts, setPosts] = useState([]);
     const [loading, setLoading] = useState(false);
     const [currentPage, setCurrentPage] = useState(1);
-    const [postsPerPage] = useState(10);
+    const [postsPerPage] = useState(9);
 
     useEffect(() => {
         const fetchPosts = async () => {
