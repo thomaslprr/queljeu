b3c7e25afad81c6c0c5f0e1df8b48fa12f409e02
Thomas Lapierre <thomaslapierre@orange.fr>
2020-04-16 16:40:05 +0200

    Correction bug affichage composant AffichageListeItem.js

--- pages/components/PageDetailJeu/AffichageListeItem.js
+++ pages/components/PageDetailJeu/AffichageListeItem.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, {useEffect,useState} from 'react'
 
 
-const AffichageListeItem = ({listeItem}) => {
+const AffichageListeItem = ({name,listeItem,preTexte}) => {
+
+    const [txt,setTxt] = useState(null);
+
+    useEffect(()=> {
+
+        val();
+
+    },[])
 
 
 
     const val = () => {
 
-        if(listeItem!='error'){
-            return listeItem.map(({id,name})=>
+        if( listeItem!= -1){
+            const res = listeItem.map(({id,name})=>
 
                 <div className="item" key={id}>
                     <div className="content">
@@ -17,20 +25,39 @@ const AffichageListeItem = ({listeItem}) => {
                 </div>
 
             )
-        }else{
-            return "";
+            setTxt(res);
         }
 
     }
 
 
+    if(txt){
+
+        if(preTexte){
+            return (
+                <div>
+
+                    {preTexte || ''}<div className="sub header ui mini horizontal divided list">{txt}</div>
+                </div>
+
+            )
+        }else{
+            return (
+                <h5 className="ui header">
+                    {name || ''}
+                    <div className="sub header ui mini horizontal divided list">{txt}</div>
+                </h5>
+            )
+        }
 
 
-    return (
-        <div className="ui mini horizontal divided list">
-        {val()}
-        </div>
-    )
+
+    }else{
+        return <div></div>
+    }
+
+
+
 
 
 };
