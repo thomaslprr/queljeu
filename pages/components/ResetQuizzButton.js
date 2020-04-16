9b7171c0c560e96487ebc5d63ae1ac58e7bdd3a3
Thomas Lapierre <thomaslapierre@orange.fr>
2020-04-17 00:00:46 +0200

    Confirmation pour recommencer le quizz

--- pages/components/ResetQuizzButton.js
+++ pages/components/ResetQuizzButton.js
@@ -1,8 +1,19 @@
+import React from "react";
+import {Button, Popup} from "semantic-ui-react";
 
 class ResetQuizzButton extends React.Component {
 
     render() {
-        return <a className="ui red button icon circular huge" href="/findagame" ><i className="undo icon"></i></a>;
+        return (
+            <Popup
+                trigger={
+                    <Button className="ui red button icon circular huge" color='red' ><i className="undo icon"></i></Button>
+                }
+                content={<a  className="ui red button" href="/findagame">RECOMMENCER</a>}
+                on='click'
+                position='top right'
+            />
+        )
 
     }
 }
